e1e5921f4f64012d8cba1a45bbf5261d84604914
Vyacheslav Khvan <vykhvan@gmail.com>
2024-06-02 00:34:28 +0500

    Add database dependency

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,4 +1,7 @@
+from typing import Generator
+
 from app.clients.mlflow import MLflowClient
+from app.db.session import SessionLocal
 
 
 def get_mlflow_client() -> MLflowClient:
@@ -9,3 +12,19 @@ def get_mlflow_client() -> MLflowClient:
         MLflowClient: An instance of MLflowClient.
     """
     return MLflowClient()
+
+
+def get_db() -> Generator:
+    """
+    Generator function that yields a SQLAlchemy session for database interactions
+    within a context manager, ensuring that the session is properly closed after use.
+
+    Yields:
+        Session: A SQLAlchemy session object ready for database operations.
+    """
+    db = SessionLocal()
+    db.current_user_id = None  # Example of setting custom session attributes if needed
+    try:
+        yield db
+    finally:
+        db.close()
